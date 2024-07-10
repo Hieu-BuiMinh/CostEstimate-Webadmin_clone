74a516a9dbcc1e3c0cbbefe139f105e5d6dd8c3d
unknown <minhhieu2122000@gmail.com>
2024-07-10 09:12:25 +0700

    test 06

--- src/components/providers/next-auth-session-provider/index.tsx
+++ src/components/providers/next-auth-session-provider/index.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from 'next-auth/react'
 import React from 'react'
 
 function NextAuthSessionProvider({ children }: { children: React.ReactNode }) {
-	return <SessionProvider session={null}>{children}</SessionProvider>
+	return <SessionProvider>{children}</SessionProvider>
 }
 
 export default NextAuthSessionProvider
